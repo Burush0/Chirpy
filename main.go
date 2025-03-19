7e3e466013d5261e50b40c739ecdac66a5c7e687
Burush0 <burush02@gmail.com>
2025-03-19 12:50:31 +0100

    Added metrics for /app/ visits and a /reset endpoint for testing

--- main.go
+++ main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
 	fileHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
-	mux.Handle("/app/", fileHandler)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileHandler))
 	mux.HandleFunc("/healthz", healthCheck)
+	mux.HandleFunc("/metrics", apiCfg.hanlderMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
 	srv := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: mux,
 	}
-	log.Println("server is listening on port 8080")
-	err := srv.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func (cfg *apiConfig) hanlderMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
